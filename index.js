9dd453809bdfa241499c6669e54ccc434031c6e8
jose luis sanchez <muallin@gmail.com>
2016-02-21 23:27:39 +0100

    Initial script

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    oauthserver = require('oauth2-server');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.oauth = oauthserver({
+  model: require('./redis/model'),
+  grants: ['password', 'refresh_token'],
+  debug: true
+})
+
+
+app.all('/oauth/token', app.oauth.grant());
+
+app.get('/', app.oauth.authorise(), function (req, res) {
+  res.send('Secret area');
+});
+
+app.use(app.oauth.errorHandler());
+
+app.listen(3000);
